a2a0172023ae6100f75eadcc1bfa35ef03be4d45
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-28 11:48:22 -0500

    numeros green

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -5,13 +5,13 @@ const monstrarMenu = () => {
     console.log('==============================='.green);
     console.log('   Seleccione una opción'.red);
     console.log('===============================\n'.green);
-    console.log('1. Crear tarea');
-    console.log('2. Listar Tarea');
-    console.log('3. Listar tareas completadas');
-    console.log('4. Listar tareas pendientes');
-    console.log('5. Completar tarea(s)');
-    console.log('6. Borrar tarea');
-    console.log('0. Salir\n');
+    console.log(`${'1.'.green} Crear tarea`);
+    console.log(`${'2.'.green} Listar Tarea`);
+    console.log(`${'3.'.green} Listar tareas completadas`);
+    console.log(`${'4.'.green} Listar tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Borrar tarea`);
+    console.log(`${'0.'.green} Salir\n`);
 
 
   
